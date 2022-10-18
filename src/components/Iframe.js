3fd7862bb7b5336138f0881503fc448e857915d8
paulgitmum <paulosgudu@gmail.com>
2022-10-18 11:41:41 -0500

    Iframe

---
+++ src/components/Iframe.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Iframe() {
+  return (
+    <div >
+        <iframe src="https://jsonplaceholder.typicode.com/" style={{width:'100%', height: '400',frameBorder:'0'}}></iframe>
+    </div>
+  )
+}
+
+export default Iframe
